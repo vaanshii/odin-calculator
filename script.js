92d781ac579a48469131dce6f08b1dcef00365d5
Ivan Russel <russelblanquiza@gmail.com>
2025-03-20 10:00:48 +0800

    Added dispay input on screen. Coded responsive number shirnks when digits gets larger.

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function fitDigitsToScreen() {
+    const display = document.querySelector("#display");
+    const screen = document.querySelector(".screen");
+
+    let fontSize = 3;
+    display.style.fontSize = fontSize + "em";
+
+    while(display.scrollWidth > screen.clientWidth && fontSize > 1) {
+        fontSize -= 0.1;
+        display.style.fontSize = fontSize + "em";
+    }
+
+}
+
+display.addEventListener("change", fitDigitsToScreen());
+
+// window.addEventListener("resize", fitDigitsToScreen());
+
+
+
